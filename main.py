de4ad9462b6c4ad0d0ef57541e547c60303e2d6d
llkhs <llkhs@qq.com>
2020-12-08 14:09:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
                 token = campus.user_info["sessionId"]
                 driver.get('https://reportedh5.17wanxiao.com/collegeHealthPunch/index.html?token=%s#/punch?punchId=180'%token)
                 #time.sleep(10)
-                response = check_in(loginJson["classId"],loginJson["classDescription"],loginJson["stuNo"],loginJson["username"],phone[index],emergencyContact[index],guardianPhone[index],loginJson["userId"],loginJson["customerId"],token)
+                response = check_in(loginJson["classId"],loginJson["classDescription"],loginJson["stuNo"],loginJson["username"],phone[index],loginJson["emergencyContact"],guardianPhone[index],loginJson["userId"],loginJson["customerId"],token)
                 if  response.json()["msg"] == '成功'and index == 0:
                     strTime = GetNowTime()
                     success.append(value[-4:])
@@ -92,7 +92,7 @@ def GetNowTime():
     return strTime
 
 #打卡参数配置函数
-def GetUserJson(deptId,text,stuNum,userName,phone,emergencyContact1,guardianPhone,userId,customerId,token):
+def GetUserJson(deptId,text,stuNum,userName,phone,emergencyContact,guardianPhone,userId,customerId,token):
     return  {
 	"businessType": "epmpics",
 	"method": "submitUpInfo",
@@ -140,7 +140,7 @@ def GetUserJson(deptId,text,stuNum,userName,phone,emergencyContact1,guardianPhon
 			},
 			{
 			"propertyname": "emergencyContact",
-			"value": emergencyContact1
+			"value": emergencyContact
 			}, 
 			{
 			"propertyname": "mergencyPeoplePhone",
@@ -152,9 +152,9 @@ def GetUserJson(deptId,text,stuNum,userName,phone,emergencyContact1,guardianPhon
 }
 
 #打卡提交函数
-def check_in(deptId,text,stuNum,userName,phone,emergencyContact1,guardianPhone,userId,customerId,token):
+def check_in(deptId,text,stuNum,userName,phone,emergencyContact,guardianPhone,userId,customerId,token):
     sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
-    jsons=GetUserJson(deptId,text,stuNum,userName,phone,emergencyContact1,guardianPhone,userId,customerId,token)
+    jsons=GetUserJson(deptId,text,stuNum,userName,phone,emergencyContact,guardianPhone,userId,customerId,token)
     #提交打卡
     response = requests.post(sign_url, json=jsons,)
     return response
